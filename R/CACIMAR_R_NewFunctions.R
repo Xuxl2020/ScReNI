b4ccaae4506e045dfcc5b6f9a571046079956fda
Xuxl2020 <74811875+Xuxl2020@users.noreply.github.com>
2024-10-08 14:21:01 +0800

    Update CACIMAR_R_NewFunctions.R

--- R/CACIMAR_R_NewFunctions.R
+++ R/CACIMAR_R_NewFunctions.R
@@ -1,47 +1,4 @@
-###source('/data2/jwang/Retina/RetReg/CellType/Programs/CACIMAR_R_NewFunctions.R')
-
-
-enrich_module <- function(Kmeans_result=Kmeans_clustering_ENS, org.db=org.Gg.eg.db, enrich.db,fun_num = 5,
-                          pvalueCutoff = 0.05, use_internal_data = TRUE, organism = NULL) {
-  all_gene <- Kmeans_result
-  all_gene<-all_gene[order(all_gene$KmeansGroup),]
-  le<-levels(as.factor(all_gene$KmeansGroup))
-  for (i in le) {
-    acc11 <- all_gene[all_gene$KmeansGroup == i,]$NMDA.Symbol
-    gene1 <- clusterProfiler::bitr(acc11, fromType = "SYMBOL",
-                                   toType = c("ENTREZID"),
-                                   OrgDb = org.db)
-    if (enrich.db =='KEGG') {
-      k1 <- clusterProfiler::enrichKEGG(gene = gene1$ENTREZID,
-                                        pvalueCutoff = pvalueCutoff
-                                        ,organism = organism
-                                        ,use_internal_data = use_internal_data)
-    }else if(enrich.db =='GO'){
-      k1 = clusterProfiler::enrichGO(gene = gene1$ENTREZID,
-                                     OrgDb = org.db,
-                                     keyType = "ENTREZID",
-                                     ont = "BP",
-                                     pvalueCutoff = pvalueCutoff)
-    }
-    acc2 <- k1@result
-    acc2$'-log10(q-value)' <- -log10(acc2$qvalue)
-    acc2 <- acc2[order(-acc2$`-log10(q-value)`),]
-    if (i=='1' | i == 1) {
-      acc21 <- acc2[1:fun_num,]
-      acc21$module <- rep(i,fun_num)
-    }else{
-      acc22 <- acc2[1:fun_num,]
-      acc22$module<-rep(i,fun_num)
-      acc21 <- rbind(acc21,acc22)
-    }
-  }
-  acc21 <- acc21[,c(1,2,11,10,3:9)]
-  return(acc21)
-}
-
-
-
-
+#' @export
 Str_to_GR <- function(x){
   sp = strsplit(x,split='-')
   chr = sapply(sp,function(x) x[[1]])
@@ -50,22 +7,22 @@ Str_to_GR <- function(x){
   GR_out = GRanges(chr,IRanges(s,e))
   return(GR_out)
 }
-
+#' @export
 Seqnames <- function(x){
   out= as.character(seqnames(x))
   return(out)
 }
-
+#' @export
 Start <- function(x){
   out= as.numeric(start(x))
   return(out)
 }
-
+#' @export
 End <- function(x){
   out= as.numeric(end(x))
   return(out)
 }
-
+#' @export
 Must_to_GR <- function(x){
   library('pbapply')
   chr_all = pblapply(x,Seqnames)
@@ -81,7 +38,7 @@ Must_to_GR <- function(x){
   GR_out = GRanges(chr_all,IRanges(start_all,end_all),motifs=names_all)
   return(GR_out)
 }
-
+#' @export
 motifs_select <- function(motif,gene){
   index <- c()
   if (stringr::str_sub(gene[1],1,3)=='ENS') {
@@ -137,21 +94,21 @@ identify_region_tfs <- function(GR,gene.use,motifdb,
   regulation <- data.frame('TF'=enriched_tf,'Target'=names(enriched_tf))
   return(regulation)
 }
-
+#' @export
 overlap_peak_motif <- function(peak,motif,motifdb){
   overlaped = findOverlaps(peak,motif)
   peak_motif = cbind(as.data.frame(peak[overlaped@from]),as.data.frame(motif[overlaped@to]))
   peak_motif$TF = motifdb[match(peak_motif$motifs,motifdb$Accession),4]
   return(peak_motif)
 }
-
+#' @export
 make_tf_target <- function(atac_out){
   tf = atac_out$TF
   tf = paste0(tf,'#',atac_out$symbol)
   tf_target = unlist(map(tf,~paste_gene(.x)))
   return(tf_target)
 }
-
+#' @export
 paste_gene <- function(gene){
   tf = strsplit(gene,'#')[[1]][1]
   target = strsplit(gene,'#')[[1]][2]
@@ -159,7 +116,15 @@ paste_gene <- function(gene){
   return(paste0(tf,'-',target))
 }
 
-
+#' Title
+#'
+#' @param fimo_regulation
+#' @param regulatory_relationships
+#' 
+#' @return
+#' @export
+#'
+#' @examples
 filter_regulation_fimo <- function(fimo_regulation,regulatory_relationships){
   if (!'TF' %in% colnames(regulatory_relationships)) {
     stop('regulatory_relationships should contain "TF" column')
@@ -178,47 +143,7 @@ filter_regulation_fimo <- function(fimo_regulation,regulatory_relationships){
   regulation1 <- regulatory_relationships[regulation_pair %in% fimo_pair,]
   return(regulation1)
 }
-
+#' @export
 split_motif <- function(motif){
   gene1 <- strsplit(motif[5],';')[[1]]
 }
-
-enrich_module <- function(Kmeans_result=Kmeans_clustering_ENS, org.db=org.Gg.eg.db, enrich.db ,fun_num = 5,
-                          pvalueCutoff = 0.05, use_internal_data = TRUE, organism = NULL) {
-  all_gene <- Kmeans_result
-  all_gene<-all_gene[order(all_gene$KmeansGroup),]
-  le<-levels(as.factor(all_gene$KmeansGroup))
-  for (i in le) {
-    i=1
-    acc11<-rownames(all_gene[all_gene$KmeansGroup == i,])
-    gene1 <- clusterProfiler::bitr(acc11, fromType = "ENSEMBL",
-                                   toType = c("SYMBOL", "ENTREZID"),
-                                   OrgDb = org.db)
-    if (enrich.db =='KEGG') {
-      k1 <- clusterProfiler::enrichKEGG(gene = gene1$ENTREZID,
-                                        pvalueCutoff = pvalueCutoff
-                                        ,organism = organism
-                                        ,use_internal_data = use_internal_data)
-    }else if(enrich.db =='GO'){
-      k1 = clusterProfiler::enrichGO(gene = gene1$ENTREZID,
-                                     OrgDb = org.db,
-                                     keyType = "ENTREZID",
-                                     ont = "BP",
-                                     pvalueCutoff = pvalueCutoff)
-    }
-    acc2 <- k1@result
-    acc2$'-log10(q-value)' <- -log10(acc2$qvalue)
-    acc2 <- acc2[order(-acc2$`-log10(q-value)`),]
-    if (i=='1' | i == 1) {
-      acc21 <- acc2[1:fun_num,]
-      acc21$module <- rep(i,fun_num)
-    }else{
-      acc22 <- acc2[1:fun_num,]
-      acc22$module<-rep(i,fun_num)
-      acc21 <- rbind(acc21,acc22)
-    }
-  }
-  acc21 <- acc21[,c(1,2,11,10,3:9)]
-  return(acc21)
-}
-

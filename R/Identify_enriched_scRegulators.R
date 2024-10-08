968941c3e2d1443d5ef537fca7ee9819e74cdbaf
Xuxl2020 <74811875+Xuxl2020@users.noreply.github.com>
2024-10-08 10:50:01 +0800

    Update Identify_enriched_scRegulators.R

--- R/Identify_enriched_scRegulators.R
+++ R/Identify_enriched_scRegulators.R
@@ -1,4 +1,15 @@
-###Identify regulators enriched in each single-cell regulatory network
+
+#' Identify regulators enriched in each single-cell regulatory network
+#'
+#' @param regulatory_network
+#' @param Kmeans_clustering_ENS
+#' @param TFFDR1
+#' @param TFFDR2
+#' 
+#' @return
+#' @export
+#'
+#' @examples
 
 Identify_enriched_scRegulators <- function(regulatory_network, Kmeans_clustering_ENS, TFFDR1 = 10, TFFDR2 = 10){
   library(IReNA)
